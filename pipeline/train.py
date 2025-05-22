921db6779b1bfd4db895664066cb8ba770a5f8a8
devery59 <kunhee9045@gmail.com>
2025-05-22 16:44:23 +1000

    Evaluate Model Performace

--- pipeline/train.py
+++ pipeline/train.py
@@ -5,7 +5,9 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error,r2_score
+import numpy as np
+
 
 
 @task
@@ -19,14 +21,26 @@ def train_model(df: pd.DataFrame):
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
     with mlflow.start_run():
+        # Model Training
         model = LinearRegression()
         model.fit(X_train, y_train)
 
-        predictions = model.predict(X_val)
-        mse = mean_squared_error(y_val, predictions)
+        # Predictions
+        y_pred = model.predict(X_val)
 
+        # Metrics Calculation
+        mse = mean_squared_error(y_val, y_pred)
+        rmse = np.sqrt(mse)
+        r2 = r2_score(y_val, y_pred)
+
+        # Log
         mlflow.log_param("model_type", "LinearRegression")
         mlflow.log_metric("mse", mse)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+
+        # Save Model
         mlflow.sklearn.log_model(model, "model")
+
 
     return mse

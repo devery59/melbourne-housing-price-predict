bac69ad0eac3d6327ab9e935fc77957d401f144b
devery59 <kunhee9045@gmail.com>
2025-05-22 16:39:29 +1000

    Verify and Connect MLflow UI

--- pipeline/train.py
+++ pipeline/train.py
@@ -1,5 +1,3 @@
-# pipeline/train.py
-
 from prefect import task, flow
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -7,9 +5,14 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+
 
 @task
 def train_model(df: pd.DataFrame):
+    # MLflow Setting
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("melbourne-housing-price")
     X = df.drop("Price", axis=1)
     y = df["Price"]
     
@@ -22,7 +25,6 @@ def train_model(df: pd.DataFrame):
         predictions = model.predict(X_val)
         mse = mean_squared_error(y_val, predictions)
 
-        # 로그 남기기
         mlflow.log_param("model_type", "LinearRegression")
         mlflow.log_metric("mse", mse)
         mlflow.sklearn.log_model(model, "model")
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+import os
 from pipeline.ingest import ingest_flow
 from pipeline.preprocess import preprocess_flow
+from pipeline.train import train_model
 
 if __name__ == "__main__":
     raw_df = ingest_flow()
     clean_df = preprocess_flow(raw_df)
-    
-    print(clean_df.head())
+    mse = train_model(clean_df)
+    print(f"✅ Training complete. MSE: {mse}")
